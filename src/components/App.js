6236392c5a5f4abcf78d90163ff8d90439ad00c5
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2021-11-02 14:52:12 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,43 @@ class App extends React.Component {
     }
   }
 
+  onChangeType = (event) => {
+    const type = event.target.value
+
+    this.setState({
+      filters: {
+        type: type
+      }
+    })
+  }
+
+  // Another way using spread operator
+  // onChangeType = ({ target: { value } }) => {
+  //   this.setState({ filters: { ...this.state.filters, type: value } });
+  // };
+
+  onFindPetsClick = (event) => {
+    if(this.state.filters.type === 'all') {
+    fetch('/api/pets')
+    .then(resp => resp.json())
+    .then(data => this.setState({
+      pets: data
+    }))
+  } else {
+    fetch(`/api/pets?type=${this.state.filters.type}`)
+    .then(resp => resp.json())
+    .then(data => this.setState({
+      pets: data
+    }))
+  }
+}
+
+  onAdoptPet = (id) => {
+    this.state.pets.find(pet => pet.id === id).isAdopted = true
+  }
+
+
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +61,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.onChangeType} onFindPetsClick={this.onFindPetsClick}/>
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser onAdoptPet={this.onAdoptPet} pets={this.state.pets} />
             </div>
           </div>
         </div>
